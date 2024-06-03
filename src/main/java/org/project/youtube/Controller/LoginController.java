ed938876113d91d72eb96275ea88749c8608b3da
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2024-06-03 22:09:20 +0330

    feat: login-view, LoginController refactor: MainController main panel design updated

---
+++ src/main/java/org/project/youtube/Controller/LoginController.java
@@ -0,0 +1,5 @@
+package org.project.youtube.Controller;
+
+public class LoginController {
+
+}
--- src/main/java/org/project/youtube/Controller/MainController.java
+++ src/main/java/org/project/youtube/Controller/MainController.java
@@ -6,22 +6,33 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class MainController {
+
     @FXML
     private ImageView mainLogo;
     @FXML
     private TextField searchBox;
     @FXML
     private Button searchButton;
+    @FXML
+    private VBox sideBarBox;
+    @FXML
+    private Button signInButton;
 
     @FXML
-    void refreshPage(MouseEvent event) {
+    void refreshAll(MouseEvent event) {
 
     }
 
     @FXML
     void searchAll(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signIn(ActionEvent event) {
 
     }
 }
