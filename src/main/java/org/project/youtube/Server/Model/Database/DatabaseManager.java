27d94974b53ae5c6dd8c96bfb94932a246961f2c
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2024-06-28 12:55:53 +0330

    fix: DatabaseManager removed DatabaseConfig and fixed connection

--- src/main/java/org/project/youtube/Server/Model/Database/DatabaseManager.java
+++ src/main/java/org/project/youtube/Server/Model/Database/DatabaseManager.java
@@ -10,11 +10,16 @@ import java.util.List;
 import java.util.UUID;
 
 public class DatabaseManager {
-    private static final String JDBC_URL = DatabaseConfig.getUrl();
-    private static final String USERNAME = DatabaseConfig.getUser();
-    private static final String PASSWORD = DatabaseConfig.getPassword();
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/youtube";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "2005tmsv";
 
     private static Connection connect() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
     }
 
