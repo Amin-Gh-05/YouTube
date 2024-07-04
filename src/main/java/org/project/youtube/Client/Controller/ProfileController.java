29b4855a9c6c311a41701ef6fb79e4ab3d50bcd0
Sepide <persianalba@gmail.com>
2024-07-05 00:30:18 +0330

    feat: controlsfx library added

--- src/main/java/org/project/youtube/Client/Controller/ProfileController.java
+++ src/main/java/org/project/youtube/Client/Controller/ProfileController.java
@@ -14,6 +14,7 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import org.project.youtube.Client.Main;
 import org.project.youtube.Client.Model.User;
+import org.controlsfx.control.Notifications;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
