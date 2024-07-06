deffc1a7f3989870dede1fc09b39601daa62e88e
Amin Ghoorchian <mo.ghoorchian@gmail.com>
2024-07-06 23:42:00 +0330

    reformat: channel, playlist style and redesign

--- src/main/java/org/project/youtube/Client/Controller/PlaylistController.java
+++ src/main/java/org/project/youtube/Client/Controller/PlaylistController.java
@@ -3,16 +3,29 @@ package org.project.youtube.Client.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 
 public class PlaylistController {
     MainController controller;
 
     @FXML
-    private Label descriptionLabel;
+    private ImageView playlistImage;
 
     @FXML
     private Label nameLabel;
 
     @FXML
-    private ImageView playlistImage;
+    private Label handleLabel;
+
+    @FXML
+    private Label isPublicLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private FlowPane videosPanel;
+
+    @FXML
+    private FlowPane shortsPanel;
 }
