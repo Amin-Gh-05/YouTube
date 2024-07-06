f687b26444a7482dcfdf296acb818dac8b0098af
AmirAli Araghi <amirali40223@gmail.com>
2024-07-06 13:09:11 +0330

    feat: API - Create - Uploader File Name

--- src/main/java/org/project/youtube/Client/Model/Network/FileTransfer.java
+++ src/main/java/org/project/youtube/Client/Model/Network/FileTransfer.java
@@ -1,10 +1,11 @@
 package org.project.youtube.Client.Model.Network;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class FileTransfer {
-    public static void sendFile(String path) {
-        new Thread(new Uploader(path)).start();
+    public static void sendFile(String path, UUID ID) {
+        new Thread(new Uploader(path, ID)).start();
     }
 
     public static void getFile() throws IOException {
