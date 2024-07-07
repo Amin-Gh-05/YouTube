b94fce88661e3b8a4ac2d91934d47e2b72c72703
AmirAli Araghi <amirali40223@gmail.com>
2024-07-07 12:41:34 +0330

    feat: MediaPlayerFadeOut Thread

---
+++ src/main/java/org/project/youtube/Client/Controller/MediaPlayerFadeOut.java
@@ -0,0 +1,34 @@
+package org.project.youtube.Client.Controller;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+public class MediaPlayerFadeOut implements Runnable {
+    @FXML
+    private VBox vBox;
+    public MediaPlayerFadeOut(VBox vBox){
+        this.vBox = vBox;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Thread.sleep(1500);
+                Platform.runLater(this::hide);
+            }
+        }
+        catch (InterruptedException e) {
+        }
+    }
+
+    public void hide() {
+        FadeTransition ft = new FadeTransition(Duration.millis(600), vBox);
+        ft.setFromValue(vBox.getOpacity());
+        ft.setToValue(0.0);
+        ft.play();
+    }
+}
