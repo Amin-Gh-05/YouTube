0839c0ea0f6d70dcd64de6dc17e53ac3267ae25c
AmirAli Araghi <amirali40223@gmail.com>
2024-07-08 01:22:10 +0330

    feat: short-player

---
+++ src/main/java/org/project/youtube/Client/Controller/ShortPlayerController.java
@@ -0,0 +1,13 @@
+package org.project.youtube.Client.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ShortPlayerController {
+    @FXML
+    private Button playBtn;
+
+    public void playBtnAction(ActionEvent actionEvent) {
+    }
+}
