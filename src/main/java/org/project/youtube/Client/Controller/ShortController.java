9068584f23bb6289caaf930ce00686cd6bbbbcdb
AmirAli Araghi <amirali40223@gmail.com>
2024-07-08 20:30:18 +0330

    feat: ShortController button Clicked Effect

--- src/main/java/org/project/youtube/Client/Controller/ShortController.java
+++ src/main/java/org/project/youtube/Client/Controller/ShortController.java
@@ -2,11 +2,43 @@ package org.project.youtube.Client.Controller;
 
 import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.util.Duration;
 
 public class ShortController {
+    @FXML
+    private Button commentsBtn;
+
+    @FXML
+    private BorderPane commentsBtnBorderPane;
+
+    @FXML
+    private Button dislikeBtn;
+
+    @FXML
+    private BorderPane dislikeBtnBorderPane;
+
+    @FXML
+    private Button likeBtn;
+
+    @FXML
+    private BorderPane likeBtnBorderPane;
+
+    @FXML
+    private HBox mainHBox;
+
+    @FXML
+    private Button nextBtn;
+
+    @FXML
+    private Button previousBtn;
+
+    @FXML
+    private Button saveToPLBtn;
 
     private void playClickEffect(Button button) {
         // animation class
@@ -24,36 +56,42 @@ public class ShortController {
     }
 
     public void nextBtnAction(ActionEvent actionEvent) {
+        playClickEffect(nextBtn);
     }
     public void nextBtnBorderPaneMouseClicked(MouseEvent mouseEvent) {
     }
 
 
     public void previousBtnAction(ActionEvent actionEvent) {
+        playClickEffect(previousBtn);
     }
     public void previousBtnBorderPaneMouseClicked(MouseEvent mouseEvent) {
     }
 
 
     public void likeBtnAction(ActionEvent actionEvent) {
+        playClickEffect(likeBtn);
     }
     public void likeBtnBorderPaneMouseClicked(MouseEvent mouseEvent) {
     }
 
 
     public void dislikeBtnAction(ActionEvent actionEvent) {
+        playClickEffect(dislikeBtn);
     }
     public void dislikeBtnBorderPaneMouseClicked(MouseEvent mouseEvent) {
     }
 
 
     public void commentsBtnAction(ActionEvent actionEvent) {
+        playClickEffect(commentsBtn);
     }
     public void commentsBtnBorderPaneMouseClicked(MouseEvent mouseEvent) {
     }
 
 
     public void saveToPLBtnAction(ActionEvent actionEvent) {
+        playClickEffect(saveToPLBtn);
     }
     public void saveToPLBtnBorderPaneMouseClicked(MouseEvent mouseEvent) {
     }
